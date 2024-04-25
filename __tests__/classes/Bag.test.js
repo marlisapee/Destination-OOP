62f72b4179fc233a825e653eb62d25bb85010072
marlisapee <mpee@tripadvisor.com>
2024-04-25 14:41:29 -0400

    created bag class and tests

---
+++ __tests__/classes/Bag.test.js
@@ -0,0 +1,40 @@
+const Bag = require('../../classes/Bag');
+
+describe('Bag class verification', () => {
+  let bag1;
+  let bag2;
+
+  beforeEach(() => {
+    bag1 = new Bag(10, 1);
+    bag2 = new Bag(11, 2);
+  });
+
+  test('Can create an instance of the Bag class property', () => {
+    expect(bag1 instanceof Bag).toBe(true);
+    expect(bag2 instanceof Bag).toBe(true);
+  });
+
+  test('Constructor should properly initialize the weight and id properties', () => {
+    expect(bag1.weight).toBe(10);
+    expect(bag2.weight).toBe(11);
+    expect(bag1.id).toBe(1);
+    expect(bag2.id).toBe(2);
+  });
+
+  test('Every bag has an owner initialized to null', () => {
+    expect(bag1.owner).toBe(null);
+    expect(bag2.owner).toBe(null);
+  });
+
+  test('getOwner() successfully returns the owner of the bag', () => {
+    expect(bag1.getOwner()).toBe(bag1.owner);
+    expect(bag2.getOwner()).toBe(bag2.owner);
+  });
+
+  test('assignOwner() successfully assigns an owner to a bag', () => {
+    bag1.assignOwner('Mar');
+    bag2.assignOwner('Jane');
+    expect(bag1.owner).toBe('Mar');
+    expect(bag2.owner).toBe('Jane');
+  });
+});
--- classes/Bag.js
+++ classes/Bag.js
@@ -1,7 +1,17 @@
 class Bag {
+  constructor(weight, id) {
+    this.weight = weight;
+    this.id = id;
+    this.owner = null;
+  }
 
+  getOwner() {
+    return this.owner;
+  }
 
+  assignOwner(name) {
+    this.owner = name;
+  }
 }
-
 
 module.exports = Bag;
